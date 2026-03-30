fd560eb31eb0996f8686723027a1b94fd6e682ee
Mehmet Samet Duman <yongdohyun@projecttick.org>
2026-03-30 21:46:31 +0300

    NOISSUE updated old api to new api
    Signed-off-by: Mehmet Samet Duman <yongdohyun@projecttick.org>

--- cmake/QMakeQuery.cmake
+++ cmake/QMakeQuery.cmake
@@ -6,9 +6,12 @@ set(__QMAKEQUERY_CMAKE__ TRUE)
 get_target_property(QMAKE_EXECUTABLE Qt6::qmake LOCATION)
 
 function(QUERY_QMAKE VAR RESULT)
-    exec_program(${QMAKE_EXECUTABLE} ARGS "-query ${VAR}" RETURN_VALUE return_code OUTPUT_VARIABLE output )
-    if(NOT return_code)
+    execute_process(COMMAND ${QMAKE_EXECUTABLE} -query ${VAR}
+        RESULT_VARIABLE return_code
+        OUTPUT_VARIABLE output
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(return_code EQUAL 0)
         file(TO_CMAKE_PATH "${output}" output)
         set(${RESULT} ${output} PARENT_SCOPE)
-    endif(NOT return_code)
+    endif()
 endfunction(QUERY_QMAKE)
